319bffc87db57cb55e4d698e75822e54e7852a0b
Jhon <jhondharkyson@gmail.com>
2025-01-09 07:30:11 -0300

    Fix: Update Gemini API prompt for better results

--- src/services/CreateNutritionService.ts
+++ src/services/CreateNutritionService.ts
@@ -11,12 +11,12 @@ class CreateNutritionService {
                 atualmente nível de atividade: ${level} e ignore qualquer outro parametro que não seja os passados, 
                 retorne em json com as respectivas propriedades, propriedade nome o nome da pessoa, propriedade sexo 
                 com sexo, propriedade idade, propriedade altura, propriedade peso, propriedade objetivo com o objetivo 
-                atual, propriedade refeições com uma array contendo dentro cada objeto sendo uma refeição da dieta e 
+                atual(em pt-br), propriedade refeições com uma array contendo dentro cada objeto sendo uma refeição da dieta e 
                 dentro de cada refeição a propriedade horário com horário da refeição, propriedade nome com nome e a 
-                propriedade alimentos com array contendo os alimentos dessa refeição e pode incluir uma propreidade 
+                propriedade alimentos com array contendo os alimentos dessa refeição e pode incluir uma propriedade 
                 como suplementos contendo array com sugestão de suplemento que é indicado para o sexo dessa pessoa e 
                 o objetivo dela e não retorne nenhuma observação alem das passadas no prompt, retorne em json e 
-                nenhuma propriedade pode ter acento.`);
+                nenhum nome de propriedade pode ter acento, porém o resultado das propriedades, devem conter acentução e letra maiúscula no inicio.`);
             
             //console.log(JSON.stringify(response, null, 2));
 
